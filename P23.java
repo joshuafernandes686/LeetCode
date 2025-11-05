fe60d605e4cba8880b1ece345ef046f326078ec8
joshuafernandes686 <joshuafernandes686@gmail.com>
2025-11-05 09:45:33 +0530

    new_program

--- P23.java
+++ P23.java
@@ -1,3 +1,8 @@
+/*MERGE K LINKED LISTS
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ */
+
 import java.util.*;
 
 
